dd41c33bd0a9761bbb408235fed6ea28662496dc
Matheus Tanaka <matheustanakanog@gmail.com>
2024-09-25 19:38:35 -0300

    import modules

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "ec2" {
+  source                   = "./modules/ec2/"
+  public_key               = var.public_key
+  security_group_allow_ssh = module.network.security_group_allow_ssh
+  environment              = var.environment[terraform.workspace]
+  tag_name                 = var.tag_name[terraform.workspace]
+  subnet_ec2_public_az_a   = module.network.subnet_ec2_public_az_a
+  subnet_ec2_public_az_b   = module.network.subnet_ec2_public_az_b
+}
+
+module "network" {
+  source      = "./modules/network/"
+  environment = var.environment[terraform.workspace]
+  tag_name    = var.tag_name[terraform.workspace]
+}
+
+module "elb" {
+  source                   = "./modules/elb/"
+  security_group_allow_ssh = module.network.security_group_allow_ssh
+  subnet_ec2_public_az_a   = module.network.subnet_ec2_public_az_a
+  subnet_ec2_public_az_b   = module.network.subnet_ec2_public_az_b
+  vpc_ec2                  = module.network.vpc_ec2
+  ec2                      = module.ec2.ec2
+}
