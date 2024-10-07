cac6e9b71ec62c3e226ca7aad9c37e8d132f9463
Matheus Tanaka <matheustanakanog@gmail.com>
2024-10-07 19:13:36 -0300

    fix auto approve

--- start.sh
+++ start.sh
@@ -4,16 +4,16 @@ echo "Starting environments"
 
 terraform workspace select development
 terraform plan
-terraform apply --auto-aprove
+terraform apply --auto-approve
 
 sleep 3;
 
 terraform workspace select testing
 terraform plan
-terraform apply --auto-aprove
+terraform apply --auto-approve
 
 sleep 3;
 
 terraform workspace select production
 terraform plan
-terraform apply --auto-aprove
+terraform apply --auto-approve
